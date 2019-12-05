769ec08eeb849d626f40b1c62670a8cfa2ae0f84
vyashole <vyashole@gmail.com>
2019-12-06 00:21:50 +0530

    boring list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { BOOKS } from '../static/books'
+import { stringify } from 'qs';
 
 export default class Home extends Component {
+
+  renderItem = ({ item }) => {
+    return (
+      <View style={styles.item}>
+        <Text>{item.title}</Text>
+      </View>
+    );
+  }
+
+  keyExtractor = book => book.id.toString()
+  
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello, World!</Text>
+        <FlatList
+          data={BOOKS}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem} />
       </View>
     )
   }
@@ -18,4 +35,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    flex: 1,
+    margin: 5
+  }
+
 });
