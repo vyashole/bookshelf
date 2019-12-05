e8b26100b120d50d77b56345796db2931a43bbf4
vyashole <vyashole@gmail.com>
2019-12-05 20:31:52 +0530

    [data] Added static books data

---
+++ src/static/books.js
@@ -0,0 +1,49 @@
+export const READING = 'reading'
+export const WANT_TO_READ = 'wantToRead'
+export const COMPLETED = 'completed'
+export default Books = [
+    {
+        id: 1,
+        title: 'Harry Potter and the Goblet of Fire',
+        author: 'J. K. Rowling',
+        thumbnail: 'https://covers.openlibrary.org/w/id/7984916-M.jpg',
+        category: READING
+    },
+    {
+        id: 2,
+        title: 'The Hobbit',
+        author: 'J. R. R. Tolkien',
+        thumbnail: 'https://covers.openlibrary.org/w/id/6979861-M.jpg',
+        category: READING
+    },
+    {
+        id: 3,
+        title: 'of 1984',
+        author: 'George Orwell',
+        thumbnail: 'https://covers.openlibrary.org/w/id/7222246-M.jpg',
+        category: WANT_TO_READ
+    },
+    {
+        id: 4,
+        title: 'The Alchemist',
+        author: 'Paulo Coelho',
+        thumbnail: 'https://covers.openlibrary.org/w/id/7222249-M.jpg',
+        category: WANT_TO_READ
+    },
+    {
+        id: 5,
+        title: 'The Great Gatsby',
+        author: 'Scott Fitzerald',
+        thumbnail: 'https://covers.openlibrary.org/w/id/7222236-M.jpg',
+        category: COMPLETED
+    },
+    {
+        id: 6,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        thumbnail: 'https://covers.openlibrary.org/w/id/7222250-M.jpg',
+        category: COMPLETED
+    }
+  ]
+  
+  
