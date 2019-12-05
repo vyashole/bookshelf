74417b1ddcc88c2b15501cb786355dadfbdf381f
vyashole <vyashole@gmail.com>
2019-12-05 21:00:44 +0530

    Discipline wala hello world

---
+++ src/components/AppNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './Home'
+
+export default function AppNavigator() {
+  return (
+    <Home/>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
